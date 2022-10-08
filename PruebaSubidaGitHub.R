1cca2d7feaa753b722befb5baccadcf8dc7ec8bf
Ale <amoncal21@gmail.com>
2022-10-08 19:19:50 +0200

    Ejemplo subida de archivo

---
+++ PruebaSubidaGitHub.R
@@ -0,0 +1,7 @@
+#Prueba de subida de archivo en GITHUB
+## comandos:
+## git clone "https..  .git delrepo"
+## git init
+## git add .
+## git commit -m "message"
+## git push origin main 'https de la pg' / git push -u origin main
