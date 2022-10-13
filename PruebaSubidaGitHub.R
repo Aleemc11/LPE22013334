6af2dde647d99a93467a23d29531e0d5269e0ae8
Ale <amoncal21@gmail.com>
2022-10-13 22:32:41 +0200

    Actualizacion con descargas de kaggle

--- PruebaSubidaGitHub.R
+++ PruebaSubidaGitHub.R
@@ -5,3 +5,9 @@
 ## git add .
 ## git commit -m "message"
 ## git push origin main 'https de la pg' / git push -u origin main
+
+
+##Descargar archivo kaggle
+## cd LPE22013334
+## kaggle datasets list -s "titanic"
+## kaggle datasets download -d 'heptapod/titanic'
